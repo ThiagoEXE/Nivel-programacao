294a0951c773431bec5dfe0260d59caf12353faf
ThiagoEXE <your-email-addressthiagob.oliveira@ucsal.edu.br>
2022-04-19 00:49:42 -0300

    cálculo de nivel

---
+++ src/calculo/nivel/Main.java
@@ -0,0 +1,9 @@
+package calculo.nivel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Nivel nivel = new Nivel(0);
+		
+	}
+}
---
+++ src/calculo/nivel/Nivel.java
@@ -0,0 +1,26 @@
+package calculo.nivel;
+
+import java.util.Scanner;
+
+public class Nivel {
+	
+	int exp;
+	int nivel = 0;
+	
+	public Nivel(int exp) {
+
+		while (true) {
+
+            this.exp =exp;
+			System.out.println("Digite a quantidade de horas estudadas:");
+			Scanner sc =new Scanner(System.in);
+			int valor = sc.nextInt();;
+			
+			if (valor >= 10) {
+				nivel++;
+			}
+			System.out.println("Você está no nível: " + nivel);
+		}
+	}
+
+}
